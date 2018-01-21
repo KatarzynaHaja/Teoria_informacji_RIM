066badeb26827f5881ec9bea12fa31fb56e8382f
KatarzynaHaja <kasia.haja@gmail.com>
2018-01-22 00:21:46 +0100

    fix bugs

--- RIM.py
+++ RIM.py
@@ -6,17 +6,18 @@ from sklearn.metrics.cluster import normalized_mutual_info_score
 from sklearn.metrics import accuracy_score
 import matplotlib.pyplot as plt
 iris = datasets.load_iris()
+from sklearn.datasets.samples_generator import make_blobs
 from scipy.optimize import minimize
 X = iris.data
 y = iris.target
+# X, y = make_blobs(n_samples=100, centers=3, n_features=2,random_state=0)
+
 
 class RIM:
     def __init__(self,X, y, K,alfa):
         self.K = K
-        self.data = np.insert(X, 0, y, axis=1)
-        self.data = np.random.permutation(self.data)
-        self.X = self.data[:,1:]
-        self.y = self.data[:,0]
+        self.X = X
+        self.y = y
 
         self.cluster_by_k_means = defaultdict(list)
         self.alfa = alfa
@@ -30,17 +31,10 @@ class RIM:
         self.X = (self.X - means)/stds
 
     def initialize(self):
-        self.kmeans = KMeans(n_clusters=self.K,init="k-means++").fit(self.X)
-        for i in range(len(self.X)):
-            self.cluster_by_k_means[self.kmeans.labels_[i]].append(self.X[i])
-
-    def test(self):
-        acc = accuracy_score(self.y, self.kmeans.labels_)
-        mi = normalized_mutual_info_score(self.y, self.kmeans.labels_)
-
+        self.kmeans = KMeans(n_clusters=self.K).fit(self.X)
 
     def sigmoid(self, y, x,lambdas):
-        lambdas = lambdas.reshape(len(self.lambdas),self.X.shape[1])
+        # lambdas = lambdas.reshape(len(self.lambdas),self.X.shape[1])
         return np.exp(np.dot(y,x))/np.sum([np.exp(np.dot(i,x)) for i in lambdas])
 
     def delta(self,k,y):
@@ -49,100 +43,132 @@ class RIM:
         else:
             return 0
 
+
     def derivative(self,lambdas):
-        lambdas = lambdas.reshape(len(self.lambdas),self.X.shape[1])
+        lambdas = lambdas.reshape(len(self.lambdas), self.X.shape[1])
         derivatives = []
         for lam in range(len(lambdas)):
-            d = []
-            for i in range(len(self.X)):
-              y = self.kmeans.labels_[i]
-              d.append(1.0/self.sigmoid(lambdas[y],self.X[i],lambdas) * self.sigmoid(lambdas[lam],self.X[i],lambdas) * self.X[i] * (self.delta(lam,y)- self.sigmoid(lambdas[y],self.X[i],lambdas)- self.alfa *2 * lambdas[lam] ))
+            der = []
+            for d in range(self.X.shape[1]):
+                s=0
+                for i in range(len(self.X)):
+                    y = self.kmeans.labels_[i]
+                    s+=1.0/self.sigmoid(lambdas[y],self.X[i],lambdas) * self.sigmoid(lambdas[lam],self.X[i],lambdas) * self.X[i][d] * (self.delta(lam,y)- self.sigmoid(lambdas[y],self.X[i],lambdas))
 
-            derivatives.append(-np.sum(np.array(d), axis =0))
+                if d!=0:
+                    s=-s+ self.alfa * 2 * lambdas[lam][d]
+                else:
+                    s=-s
+                der.append(s)
+            derivatives.append(np.array(der))
+        # print(np.array(derivatives).ravel())
         return np.array(derivatives).ravel()
 
     def cost_function(self,lambdas):
-        p =[]
         lambdas = lambdas.reshape(len(self.lambdas),self.X.shape[1])
+        reg = 0
+        for i in range(len(lambdas)):
+            for j in range(1, self.X.shape[1]):
+                reg += lambdas[i][j] ** 2
+
+        reg = self.alfa*reg
+        result = 0
         for i in range(len(self.X)):
           y = self.kmeans.labels_[i]
-          p.append(np.log(self.sigmoid(y,self.X[i],lambdas)))
-        return -np.sum(p)
+          result+=np.log(self.sigmoid(lambdas[y],self.X[i],lambdas))
+        return -(result-reg)
 
     def find_max(self):
-        self.lambdas = minimize(self.cost_function,self.lambdas , method="Newton-CG", jac=self.derivative).x.reshape(self.K, self.X.shape[1])
-        self.lambdas = self.lambdas.reshape(self.K, self.X.shape[1])
+        self.lambdas_ent =  minimize(self.cost_function,self.lambdas , method="BFGS",jac=self.derivative).x
+        print( minimize(self.cost_function,self.lambdas , method="BFGS",jac=self.derivative))
+        self.lambdas_ent = self.lambdas_ent.reshape(self.K, self.X.shape[1])
+
 
     def conditional_entropy(self,lambdas):
+        lambdas = lambdas.reshape(len(self.lambdas), self.X.shape[1])
         entropy =0
-        for j in range(len(lambdas)):
-            for i in range(len(self.X)):
+        for i in range(len(self.X)):
+            for j in range(len(lambdas)):
                 entropy +=self.sigmoid(lambdas[j],self.X[i],lambdas)*(-np.log(self.sigmoid(lambdas[j],self.X[i],lambdas)))
-            entropy = 1.0/len(self.X)*entropy
-        return entropy
+        return entropy/len(self.X)
 
     def entropy(self,lambdas):
         entropy =0
         for j in range(len(lambdas)):
-            entropy+=self.prob_class(lambdas[j],lambdas) * np.log(self.prob_class(lambdas[j],lambdas))
-        return -entropy
+            entropy+=self.prob_class(j,lambdas) * (-np.log(self.prob_class(j,lambdas)))
+        return entropy
 
     def prob_class(self,y,lambdas):
         prob =0
         for i in range(len(self.X)):
-            prob += self.sigmoid(y,self.X[i],lambdas)
-        return 1.0/len(self.X)*prob
+            prob += self.sigmoid(lambdas[y],self.X[i],lambdas)
+        return prob/len(self.X)
 
     def cost_function_rim(self,lambdas):
         lambdas = lambdas.reshape(len(self.lambdas), self.X.shape[1])
-        return -(self.entropy(lambdas) - self.conditional_entropy(lambdas))
+        reg=0
+        for i in range(len(lambdas)):
+            for j in range(1,self.X.shape[1]):
+                reg+=lambdas[i][j]**2
+        reg = self.alfa * reg
+        return -(self.entropy(lambdas) - self.conditional_entropy(lambdas) - reg)
 
     def derivative_rim(self,lambdas):
         lambdas = lambdas.reshape(len(self.lambdas), self.X.shape[1])
         derivative = []
-        for j in range(len(lambdas)):
-            s = []
-            cl = self.prob_class(lambdas[j], lambdas)
-            for i in range(len(self.X)):
-                sigm = self.sigmoid(lambdas[j],self.X[i],lambdas)
-                z = np.array(self.X[i,0]*sigm * (np.log(sigm/cl)- np.sum([self.sigmoid(lambdas[z],self.X[i],lambdas) * np.log(self.sigmoid(lambdas[z],self.X[i],lambdas)/cl) for z in range(len(lambdas))])))
-                rest = self.X[i,1:]*sigm * (np.log(sigm/cl)- np.sum([self.sigmoid(lambdas[z],self.X[i],lambdas) * np.log(self.sigmoid(lambdas[z],self.X[i],lambdas)/cl) for z in range(len(lambdas))]) * 2* self.alfa * self.lambdas[j,1:])
-                s.append(np.append(z,rest))
+        pc = [self.prob_class(c,lambdas) for c in range(len(lambdas))]
+        for k in range(len(lambdas)):
+            pk = self.prob_class(k, lambdas)
+            der = []
+            for d in range(self.X.shape[1]):
+                s = 0
 
-            derivative.append(-np.mean(s,axis=0))
+                for i in range(len(self.X)):
+                    sigm = self.sigmoid(lambdas[k], self.X[i], lambdas)
+                    s+=self.X[i][d] * sigm * (np.log(sigm / pk) - np.sum([self.sigmoid(lambdas[c], self.X[i],
+                                                                                         lambdas) * np.log(
+                    self.sigmoid(lambdas[c], self.X[i], lambdas) / pc[c]) for c in range(len(lambdas))]))
+                if d!=0:
+                    s=-s+ 2* self.alfa * self.lambdas[k][d]
+                else:
+                    s=-s
+                der.append(s/len(self.X))
+            derivative.append(np.array(der))
         return np.array(derivative).ravel()
 
     def find_max_Rim(self):
-        self.lambdas = minimize(self.cost_function_rim,self.lambdas , method="Newton-CG", jac=self.derivative_rim).x.reshape(self.K, self.X.shape[1])
-        self.lambdas = self.lambdas.reshape(self.K, self.X.shape[1])
+        self.lambdas_rim = minimize(self.cost_function_rim,self.lambdas_ent , method="BFGS",jac=self.derivative_rim).x
+        print(minimize(self.cost_function_rim,self.lambdas_ent , method="BFGS",jac=self.derivative_rim))
+        self.lambdas_rim = self.lambdas_rim.reshape(self.K, self.X.shape[1])
+
 
     def run(self):
         self.initialize()
-        print(self.lambdas)
+        print(normalized_mutual_info_score(self.y, self.kmeans.labels_))
+        print("poczatkowe",self.lambdas)
         self.find_max()
-        print(self.lambdas)
+        print("po maxEnt",self.lambdas_ent)
         self.find_max_Rim()
-        print("duuup",self.lambdas)
-        print(self.choose_class())
+        print("po rim",self.lambdas_rim)
+        classes = self.choose_class()
+        print(normalized_mutual_info_score(self.y, classes))
 
     def choose_class(self):
         tab = []
-        classes =[]
+        self.classes =[]
         for i in self.X:
             for j in range(self.K):
-                tab.append(self.sigmoid(self.lambdas[j],i,self.lambdas))
+                tab.append(self.sigmoid(self.lambdas[j],i,self.lambdas_rim))
         tab = np.array(tab).reshape(self.X.shape[0],self.K)
         for i in tab:
-            classes.append(np.argmax(i))
-        return classes
-def draw():
+            print(np.argmax(i))
+            self.classes.append(np.argmax(i))
+        return self.classes
+def draw(X,y):
     plt.scatter(X[:, 0], X[:, 1], c=y, cmap='rainbow')
     plt.show()
 
-r = RIM(X,y,3,10)
+r = RIM(X,y,2,0.2)
 r.run()
-# r.initialize()
-# #print(r.derivative(r.lambdas))
-# # draw()
-# # print(r.find_max())
-# print(r.derivative_rim(r.lambdas))
+draw(X,y)
+draw(X,r.classes)
